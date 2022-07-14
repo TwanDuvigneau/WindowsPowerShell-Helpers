c1cb327c93bbb3fdb9d5ef51c213a0f0459930fd
TwanDuvigneau <97748847+TwanDuvigneau@users.noreply.github.com>
2022-07-14 12:02:31 +0200

    Get-GroupsFromUsers

---
+++ Get-GroupsFromUsers.ps1
@@ -0,0 +1,29 @@
+﻿#Install-Module ImportExcel
+#Install-Module AzureAD
+
+Import-Module ImportExcel
+
+$ExcelPath = 'C:\temp\usergroups.xlsx'
+
+Connect-AzureAD
+
+$Users = Get-AzureADUser -All $true | Where-Object {$_.UserPrincipalName -NotLike '*EXT*'}
+
+$GroupsResults = @()
+
+Foreach ($User in $Users) {
+
+    $Groups = (Get-AzureADUserMembership -ObjectID $User.ObjectID).DisplayName
+
+    $GroupObj = [pscustomobject]@{
+        Name          = $User.DisplayName
+        UserName      = $User.UserPrincipalName
+        Department    = $User.Department
+        Function      = $User.Title
+        Groups        = $Groups -join ', '
+    }
+
+    $GroupsResults += $GroupObj
+}
+
+$GroupsResults | Export-Excel -Path $ExcelPath -WorkSheetname 'AzureAD Groups'

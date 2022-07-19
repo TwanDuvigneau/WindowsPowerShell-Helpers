ab8183e9cba4c7441e63e2544b91de47ad5f1ec7
TwanDuvigneau <97748847+TwanDuvigneau@users.noreply.github.com>
2022-07-19 18:27:26 +0200

    Update AAM-Restart-AzureVMs.ps1

--- AAM-Restart-AzureVMs.ps1
+++ AAM-Restart-AzureVMs.ps1
@@ -18,54 +18,54 @@ Param
 $RestartedMachines = [Collections.Generic.List[PSCustomObject]]::new()
 
 try {
-	$servicePrincipalConnection= Get-AutomationConnection -Name ''
+	$ServicePrincipalConnection = Get-AutomationConnection -Name ''
 
-	$AZConnection = @{
+	$ConnectAZ = @{
+		CertificateThumbprint = $ServicePrincipalConnection.CertificateThumbprint 
+		ApplicationID         = $ServicePrincipalConnection.ApplicationID
+		Tenant                = $ServicePrincipalConnection.TenantID
 		ServicePrincipal      = $true
-		TenantId              = $servicePrincipalConnection.TenantId
-		ApplicationId         = $servicePrincipalConnection.ApplicationId
-		CertificateThumbprint = $servicePrincipalConnection.CertificateThumbprint
+	}
+	Connect-AzAccount
+
+	Set-AzContext -Subscription '897aea9a-c70e-4de2-897d-ed6cd82b6a82'
+
+	Write-Verbose -Verbose "Successfully logged into Azure subscription using Az cmdlets..."
+
+	ForEach ($VirtualMachineName in $VirtualMachines) {
+
+		$GetVM = @{
+			ResourceType = "Microsoft.Compute/VirtualMachines"
+			Name         = $VirtualMachineName
+		}
+
+		$VM = Get-AzResource @GetVM
+
+		Write-Verbose -Verbose "Processing VM $($VM.Name)..."
+
+		try {
+			#Restart-AzVM -id $VM.ResourceID -Force:$true
+
+			$RestartedMachines.Add([PSCustomObject]@{
+				Action   = "Restarting Virtual Machine"
+				Resource = $($VM.Name)
+				Message  = "Succesfully restarted virtual machine $($VM.Name)"
+				IsError  = $false
+			})
+
+		} catch {
+			$RestartedMachines.Add([PSCustomObject]@{
+				Action   = "Restarting Virtual Machine"
+				Resource = $($VM.Name)
+				Message  = "Failed to restart virtual machine $($VM.Name)"
+				IsError  = $false
+				Error    = $_.Exception
+			})
+		}
+		
 	}
 
-	Add-AzAccount @AZConnection
-		
-	Write-Verbose -Verbose "Successfully logged into Azure subscription using Az cmdlets..."
+	Write-Output -InputObject $RestartedMachines
 } Catch {
 	Throw $_.Exception
 }
-
-ForEach ($VirtualMachineName in $VirtualMachines) {
-
-	$GetVM = @{
-		ResourceType = "Microsoft.Compute/VirtualMachines"
-		Name         = $VirtualMachineName
-	}
-
-	$VM = Get-AzResource @GetVM
-
-	Write-Verbose -Verbose "Processing VM $($VM.Name)..."
-
-	try {
-		#Restart-AzVM -id $VM.ResourceID -Force:$true
-
-		$RestartedMachines.Add([PSCustomObject]@{
-			Action   = "Restarting Virtual Machine"
-			Resource = $($VM.Name)
-			Message  = "Succesfully restarted virtual machine $($VM.Name)"
-			IsError  = $false
-		})
-
-	} catch {
-		$RestartedMachines.Add([PSCustomObject]@{
-			Action   = "Restarting Virtual Machine"
-			Resource = $($VM.Name)
-			Message  = "Failed to restart virtual machine $($VM.Name)"
-			IsError  = $false
-		})
-
-		Throw $_.Exception
-	}
-	
-}
-
-Write-Output -InputObject $RestartedMachines
